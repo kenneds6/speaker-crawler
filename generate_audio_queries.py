8cde3ee98ee1cd33d8c4f5c3b693a486cb4d72f9
Sean M Kennedy <seankennedy@ieee.org>
2019-02-11 11:17:35 -0500

    added tcpdump support

---
+++ generate_audio_queries.py
@@ -0,0 +1,91 @@
+import boto3
+import pandas as pd
+import config
+import argparse
+from gtts import gTTS
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("csv", help='A CSV file with queries with a column labeled Query')
+    parser.add_argument('--wake_word', help='The wake word that initiates an interaction with the device, default is '
+                                            'Alexa')
+    parser.add_argument('--voice', help='Specify which voice to use.  Defaults to the default US english google '
+                                        'female voice.  optional voices include joanna, salli, matt or joey')
+
+    args = parser.parse_args()
+
+    csv_path = args.csv
+
+    joanna = False
+    salli = False
+    matt = False
+    joey = False
+    polly = False
+
+    if args.voice == 'joanna':
+        print('Using Polly Joanna Voice')
+        joanna = True
+        polly = True
+    elif args.voice == 'salli':
+        print('Using Polly Salli Voice')
+        salli = True
+        polly = True
+    elif args.voice == 'matt':
+        print('Using Polly Matthew Voice')
+        matt = True
+        polly = True
+    elif args.voice == 'joey':
+        print('Using Polly Joey Voice')
+        joey = True
+        polly = True
+
+    default_wake_word = "Alexa "
+    if args.wake_word is not None:
+        default_wake_word = args.wake_word + " "
+
+    queries = pd.read_csv(csv_path)
+
+    for q in queries['Query']:
+        text_to_read = default_wake_word + '  ,   ' + q
+        q_file = q.replace(" ", "_")
+        if polly:
+            polly_client = boto3.Session(
+                aws_access_key_id=config.aws_access_key_id,
+                aws_secret_access_key=config.aws_secret_access_key,
+                region_name='eu-west-2').client('polly')
+            if joanna:
+                f_name = 'voice_queries/amazon/polly/joanna/_' + q_file + "_.mp3"
+                response = polly_client.synthesize_speech(VoiceId='Joanna',
+                                                          OutputFormat='mp3',
+                                                          Text=text_to_read + "              ")
+                file = open(f_name, 'wb')
+                file.write(response['AudioStream'].read())
+                file.close()
+            elif salli:
+                f_name = 'voice_queries/amazon/polly/sally/_' + q_file + "_.mp3"
+                response = polly_client.synthesize_speech(VoiceId='Salli',
+                                                          OutputFormat='mp3',
+                                                          Text=text_to_read + "              ")
+                file = open(f_name, 'wb')
+                file.write(response['AudioStream'].read())
+                file.close()
+            elif matt:
+                f_name = 'voice_queries/amazon/polly/matt/_' + q_file + "_.mp3"
+                response = polly_client.synthesize_speech(VoiceId='Matthew',
+                                                          OutputFormat='mp3',
+                                                          Text=text_to_read + "              ")
+                file = open(f_name, 'wb')
+                file.write(response['AudioStream'].read())
+                file.close()
+            elif joey:
+                f_name = 'voice_queries/amazon/polly/joey/_' + q_file + "_.mp3"
+                response = polly_client.synthesize_speech(VoiceId='Joey',
+                                                          OutputFormat='mp3',
+                                                          Text=text_to_read + "              ")
+                file = open(f_name, 'wb')
+                file.write(response['AudioStream'].read())
+                file.close()
+        else:
+            f_name = 'voice_queries/amazon/google_voice_default/_' + q_file + "_.mp3"
+            tts = gTTS(text=text_to_read, lang='en')
+            tts.save(f_name)
--- speaker_crawler.py
+++ speaker_crawler.py
@@ -1,39 +1,53 @@
-import os
+import argparse
+import csv
+import subprocess
 import time
-import pandas as pd
-from scapy.all import sniff
-from scapy.all import wrpcap
+from os import listdir
+from os.path import isfile, join
 
 
-def driver(query_path, q_time,q_name, device_ip, iterations):
+def driver(query_path, q_time, q_name, device_ip, iterations):
     for i in range(iterations):
-        # wait before starting capture
+        interface = 'br0'
+        q_time = q_time + 1
+        print('Starting capture...')
+        capture = subprocess.Popen("sudo timeout " + str(q_time) + " tcpdump -i " + str(interface) + " -n host " + str(
+            device_ip) + " -w " + "/home/pi/Desktop/speaker-crawler-master/output/" + str(q_name) + str(
+            i) + "_" + ".pcap", shell=True)
         time.sleep(1)
-        # start capture
-        start_capture(device_ip, q_name, i)
-
-        # play query
-        os.system("mpg321 " + query_path)
-        # wait for duration specified and stop and save capture
-        time.sleep(q_time)
-
-
-def start_capture(ip, trace_name='trace_', trace_number=1):
-    packets = sniff(filter='ip host ' + ip)
-    wrpcap(trace_name + trace_number + '.pcap', packets)
+        print('Playing audio query...')
+        play_audio = subprocess.Popen(["mpg321", query_path])
+        capture.wait()
+        print('Interaction captured')
+        print("Capture File Saved, iter=" + str(i))
 
 
 if __name__ == "__main__":
-    queries = pd.read_csv('/home/scramblesuit/PycharmProjects/speaker-crawler/data/echo_test/echo_test_queries.csv')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('csv',
+                        help='A CSV File that contains the columns Query and Time, corresponding to the query and '
+                             'interaction time')
+    parser.add_argument('mp3_fp', help='The directory containing the audio query files')
+    parser.add_argument('device_ip', help='The IP Address of the device')
+    parser.add_argument('iter', help='Number of capture files to generate for each query')
+    args = parser.parse_args()
 
-    query_directory = '/home/scramblesuit/PycharmProjects/speaker-crawler/voice_queries/echo_test/'
+    with open(args.csv) as csv_file:
+        q_names = []
+        q_times = []
+        queries = csv.DictReader(csv_file)
+        for row in queries:
+            q_names.append(row["Query"])
+            q_times.append(row["Time"])
 
-    from os import listdir
-    from os.path import isfile, join
+    device_ip = args.device_ip
+    query_directory = args.mp3_fp
+
     q_files = [f for f in listdir(query_directory) if isfile(join(query_directory, f))]
-    q_names = queries['Query']
-    q_times = queries['Time']
-    start_capture('192.168.86.36')
-    driver(query_directory + q_files[0], q_times[0], q_names[0], 100)
+    q_files = sorted(q_files, key=lambda s: s.casefold())
+    n_iter = int(args.iter)
+    for idx, q in enumerate(q_files):
+        driver(query_directory + q_files[idx], float(q_times[idx]), q_names[idx].replace(" ", "_") + '_', device_ip,
+               n_iter)
 
 
